877c8dea7e2e268f2a2f142e1c334b5035e5b8be
Edu Bravo <eduardobravopires@gmail.com>
2025-07-08 19:12:27 -0300

    feat: add create questions and get questions

---
+++ src/http/routes/create-question.ts
@@ -0,0 +1,42 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import z from 'zod/v4';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/index.ts';
+
+export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    '/rooms/:roomId/questions',
+    {
+      schema: {
+        body: z.object({
+          question: z.string().min(1, 'Name is required'),
+        }),
+        params: z.object({
+          roomId: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { question } = request.body;
+      const { roomId } = request.params;
+
+      const result = await db
+        .insert(schema.questions)
+        .values({
+          roomId,
+          question,
+        })
+        .returning();
+
+      const insertedQuestion = result[0];
+
+      if (!insertedQuestion) {
+        throw new Error('Failed to create Question');
+      }
+
+      return reply.status(201).send({
+        questionId: insertedQuestion.id,
+      });
+    }
+  );
+};
