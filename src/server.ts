3d3dc6bb04119998ffb3956a45a0c0ad91a63de3
Edu Bravo <eduardobravopires@gmail.com>
2025-07-07 21:13:19 -0300

    feat: create rooms list

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import cors from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-room.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(cors, {
+  origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+  return { status: 'ok' };
+});
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT }).then(() => {
+  // biome-ignore lint/suspicious/noConsole: <just in dev>
+  console.log('HTTP server running on http://localhost:3333');
+});
